0152c4c88f601136ae982ff5d27e24890211494a
Peter <piotr.nalewajka@gmail.com>
2013-07-12 20:31:32 -0400

    Adding license and module information, cutting out README brainfuck symbol list

--- Ggg.js
+++ Ggg.js
@@ -1,4 +1,34 @@
-// This is validator and compiler for language Ggg
+/*
+ * This is Ggg to javascript compiler. Look for SPEC.md for language spec.
+ * It is built after seeing http://tomi.panula-ont.to/brainfuck
+ * by Tomi Panula-Ontto.
+ *
+ * This project is made purely for learning what the author hasn't yet learned
+ * or tested, this is Turing-Complete language called Ggg, which bases 1:1 on
+ * Brainfuck esoteric language.
+ *
+ * This software is license under MIT License.
+ *
+ * Copyright (C) 2013 Piotr Nalewajka (piotr.nalewajka@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
+ */
 
 (function(Ggg, undefined) {
     
--- README.md
+++ README.md
@@ -8,24 +8,6 @@ some quite needed features.
 Valid symbols:
 Symbols in tokens: `G`, `g`, other characters are used to break sets of symbols into tokens.
 
-In brainfuck there are 8 commands:
-
-`>` increment the data pointer
-
-`<`	decrement the data pointer
-
-`+`	increment the byte at the data pointer
-    
-`-`	decrement the byte at the data pointer
-    
-`.`	output the byte at the data pointer
-
-`,`	accept one byte of input, storing its value in the byte at the data pointer
-
-`[`	if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching `]` command
-    
-`]`	if the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching `[` command
-
 In Ggg version g there are 8 basic commands which are also only valid tokens, based on those from brainfuck:
 
 `G`   as `+` (increment byte)
